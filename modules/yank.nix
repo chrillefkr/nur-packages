e898a7bc9e22de361f68c4b287a8bdd38ed505f3
Christopher Janzon <christopher.janzon@gmail.com>
2024-01-18 21:49:44 +0100

    fix(yank): allow null as yankcmd

--- modules/yank.nix
+++ modules/yank.nix
@@ -2,8 +2,9 @@
 let
   cfg = config.programs.yank;
   inherit (lib) mkEnableOption mkOption mkIf;
-  inherit (lib.types) str;
-  yank = pkgs.yank.overrideAttrs {
+  inherit (lib.types) nullOr str;
+  yank = if builtins.isNull cfg.yankcmd then pkgs.yank else
+  pkgs.yank.overrideAttrs {
     makeFlags = [ "YANKCMD=${cfg.yankcmd}" ];
   };
 in
@@ -11,7 +12,7 @@ in
   options.programs.yank = {
     enable = mkEnableOption "yank";
     yankcmd = mkOption {
-      type = str;
+      type = nullOr str;
       default = "${pkgs.wl-clipboard}/bin/wl-copy";
       description = "Command which yank runs to copy to clipboard. Examples: xclip (for Xorg), wl-copy (for Wayland), and pbcopy (for MacOS).";
       defaultText = "''${pkgs.wl-clipboard}/bin/wl-copy";
