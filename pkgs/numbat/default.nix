2ad36ab9eaf1b22c2425186e424e6f568c26cdda
Christopher Janzon <christopher.janzon@gmail.com>
2024-01-20 17:37:12 +0100

    fix(numbat): add rustc version check to meta.broken

--- pkgs/numbat/default.nix
+++ pkgs/numbat/default.nix
@@ -1,10 +1,16 @@
 { lib
 , rustPlatform
+, rustc
 , fetchFromGitHub
 , stdenv
 , darwin
 }:
-
+let
+  # package `anstream v0.6.4` cannot be built because it requires rustc 1.70.0 or newer
+  # atm, nixpkgs 23.05 uses rustc 1.69.0, and 23.11 uses 1.73.0
+  minimum-rustc = "1.70.0";
+  rustc-too-old = (builtins.compareVersions rustc.version minimum-rustc) < 0;
+in
 rustPlatform.buildRustPackage rec {
   pname = "numbat";
   version = "1.9.0";
@@ -28,5 +34,6 @@ rustPlatform.buildRustPackage rec {
     license = with licenses; [ asl20 mit ];
     maintainers = with maintainers; [ ];
     mainProgram = "numbat";
+    broken = if rustc-too-old then lib.warn "Need rustc ${minimum-rustc} or newer to build ${pname}. Consider upgrading nixpkgs to 23.11 or later" true else false;
   };
 }
