bde75849c59fb7fc1fe2b7aa08958217e2920e9c
Christopher Janzon <chrillefkr@users.noreply.github.com>
2025-09-02 11:01:25 +0200

    fix: remove dependabot, numbat & resticprofile
    No need for these packages, as they're now in nixpkgs
    
    BREAKING CHANGE:

--- pkgs.nix
+++ pkgs.nix
@@ -6,9 +6,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  numbat = pkgs.callPackage ./pkgs/numbat { };
   initramfs-tools = pkgs.callPackage ./pkgs/initramfs-tools { };
-  dependabot-cli = pkgs.callPackage ./pkgs/dependabot-cli { buildGoModule = pkgs.buildGo122Module; };
-  resticprofile = pkgs.callPackage ./pkgs/resticprofile { };
   linux-msft-wsl = pkgs.callPackage ./pkgs/linux-msft-wsl { };
 }
